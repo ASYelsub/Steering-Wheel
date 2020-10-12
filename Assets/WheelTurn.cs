d227e8ce6bc535f5fc17d69d21d8aa36537ab4fd
Abigail <asyelsub@gmail.com>
2020-10-11 23:39:33 -0400

    started blocking stuff out... too sleepy to do much else tonight

---
+++ Assets/WheelTurn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//The intent of this script is to be the input where the player presses w or d and the wheel moves
+//left or right over an amount of time which then turns the car left or right
+//theres also the "suspicion" meter the player must keep track of, that if they dont turn the wheel at the proper time
+//the human in the car will notice that they're not imapcting anything.
+public class WheelTurn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
